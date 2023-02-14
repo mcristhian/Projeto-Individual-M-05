1100cc89f7aebc7f9ac5ba4d2f0db0297407be24
mcristhian <thematheuscris@gmail.com>
2023-02-14 00:19:34 -0300

    Código implementado

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import readLineSync from 'readline-sync'
 
-console.log('teste');
+let list = [];
+let counter = 0;
+let input = '';
+
+while (input != 'sair'){
+    if (input != ''){
+        list.push(input);
+        counter++;
+    }
+    input = readLineSync.question('Digite uma propriedade CSS: ').toLocaleLowerCase();
+}
+
+// do {
+//     input = readLineSync.question('Digite uma propriedade CSS: ').toLocaleLowerCase(); 
+//     list.push(input);
+//     counter++;
+// } while (input != 'sair');
+
+console.log('\nPropriedades adicionadas:', counter);
+console.log(list.sort().join('\n'));
